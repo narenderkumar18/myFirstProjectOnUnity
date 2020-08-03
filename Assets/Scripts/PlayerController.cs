0635c4c15399934f30d72dcef4bc2616947fdfed
Narender Kumar <narender78966@gmail.com>
2020-08-04 01:50:26 +0530

    my first ever project on unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // to get input from device
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        // to move vehicle forward
+        transform.Translate(Vector3.forward* Time.deltaTime* speed * verticalInput);
+        // to turn the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
